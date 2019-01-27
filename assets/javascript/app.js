32343f6f02aa29ebc977d5a4bc4f8e3f100e236e
Dialest <dialest@hotmail.com>
2019-01-27 12:41:15 -0800

    Added ability to add buttons and pause/play gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,19 +9,29 @@ var offset = 0;
 // What we last searched for
 var last; 
 
-// make buttons for everything in the starter array
-for (var i = 0; i < buttonArr.length; i++) {
-  makeButton(buttonArr[i]);  
-}
-
 // function that makes the search buttons
 function makeButton(text) {
   var button = $("<button class='search' id=" + text + " >" + text + "</button>");
   $("#buttons").append(button);
-}
+};
+
+// make buttons for everything in the starter array
+for (var i = 0; i < buttonArr.length; i++) {
+  makeButton(buttonArr[i]);  
+};
+
+// make a button for things the user wants a button for
+$("#user-search").on("click", function (event) {
+  // don't do what you think you gonna do, button!
+  event.preventDefault();
+  var text = $("#user-value").val()
+  // don't want to make no empty buttons
+  if (text != ""){makeButton(text);}
+});
 
 // what happens when you click the search buttons
-$(".search").on("click", function () {
+$("#buttons").on("click", ".search", function () {
+  console.log("click " + $(this).attr("id"));
   offset = 0;
   last = $(this).attr("id");
   // clear everything out
@@ -35,12 +45,13 @@ $(".search").on("click", function () {
   offset = 10;
 });
 
+// how to get more gifs with the same subject
 $("#more").on("click", function () {
   gifPull(last);
   offset += 10;
-})
+});
 
-
+// how to pull gifs off giphy with their api
 function gifPull(search){
   var api_key = "xrHqiENtLdnqqycULZMd6COgbQESDkWV";
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&offset=" + offset + "&api_key=" + api_key + "&limit=10";
@@ -56,9 +67,25 @@ function gifPull(search){
       var gif = $(" \
         <div class=col-md-4> \
           <p>Rating: " + response.data[i].rating.toUpperCase() + "</p>\
-          <img src=" + response.data[i].images.fixed_height.url + " /> \
+          <img src=" + response.data[i].images.fixed_height_still.url +
+          " data-still=" + response.data[i].images.fixed_height_still.url + 
+          " data-animate=" +  response.data[i].images.fixed_height.url + 
+          " data-state='still' class='gif' /> \
         </div>");
       $("#gifs").append(gif);
     };
   });
-}
+};
+
+// if you click on a gif, it will animate or stop
+$("#gifs").on("click", ".gif", function () {
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("data-state", "animate");
+    $(this).attr("src", $(this).attr("data-animate"));
+  }
+  else {
+    $(this).attr("data-state", "still");
+    $(this).attr("src", $(this).attr("data-still"));
+  }
+});
